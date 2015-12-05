b1b3f0fe57e862975ffdb121e75da2391efac340
Jino Noel <jinonoel@gmail.com>
2015-12-05 17:01:30 +0800

    Initial Commit

---
+++ Boto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Boto
+//
+//  Created by Joseph Christian Noel on 5/12/2015.
+//  Copyright © 2015 Joseph Christian Noel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
